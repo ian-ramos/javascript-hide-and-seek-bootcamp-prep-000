c8848c94c46ef8b283849e93820e53d5c6916146
Ian Ramos <ramos.ianc@gmail.com>
2017-06-10 20:01:15 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,5 @@ function increaseRankBy(n) {
   }
 }
 function deepestChild() {
-  var main = document.querySelector('#grand-node')
-  return main.childern[3]
+  return document.querySelector('#grand-node div div div div')
 }
